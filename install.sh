37403b2960ceadd8a435a6d7d7659890400686d0
Maddison Lee Karlsdottir <maddison@hyperion>
2017-07-08 15:02:38 -0700

    adding vim colors and updating xresources

--- install.sh
+++ install.sh
@@ -4,3 +4,5 @@ mkdir -p ~/.config/i3/
 cp .config/i3/config ~/.config/i3/config
 cp .config/compton.conf ~/.config/compton.conf
 cp .Xresources ~/.Xresources
+mkdir -p ~/.vim/colors
+cp .vim/colors/solarized.vim ~/.vim/colors/solarized.vim
